dd96fa4b007049ee2945dfdf03df48557d51f8b1
Ashutosh Karna <ashutoshkarna@Ashutoshs-MacBook-Air.local>
2025-04-09 01:19:57 +0200

    fix: issue with similarity key in response

--- app/main.py
+++ app/main.py
@@ -10,8 +10,15 @@ descriptions = data['description'].apply(str).tolist()
 company_ids = data['company_id'].tolist()
 embeddings = generate_embeddings(descriptions)
 
+
+@app.get("/health")
+def ping():
+    return True
+
+
 @app.get("/retrieve_similar_companies/{company_id}")
 def retrieve_similar_companies(company_id: int):
+    # TODO: handle exception
     if company_id not in company_ids:
         raise HTTPException(status_code=404, detail="Company ID not found")
 
@@ -24,7 +31,7 @@ def retrieve_similar_companies(company_id: int):
     return {
         "company_id": company_id,
         "similar_companies": [
-            {"id": comp[0], "similarity": round(comp[1], 4)}
+            {"id": comp[0], "similarity": round(float(comp[1]), 4)}
             for comp in top_similar_companies if comp[0] != company_id  # Exclude self-match
         ]
     }
